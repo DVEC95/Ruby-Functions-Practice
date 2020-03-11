b1f3309f5dc243409e2eab4369879805027be876
David Eastcroft <davideastcroft95@gmail.com>
2020-03-11 12:07:36 +0000

    Adds fahrenheit to celsius function and test.

--- ruby_functions_practice.rb
+++ ruby_functions_practice.rb
@@ -97,7 +97,7 @@ def volume_of_sphere(radius)
   return volume.round(2)
 end
 
-# def fahrenheit_to_celsius(temp)
-#   celsius = (temp - 32) * (5.0 / 9.0)
-#   return celsius.round(2)
-# end
+def fahrenheit_to_celsius(fahrenheit)
+  celsius = (fahrenheit - 32) * (5.0 / 9.0)
+  return celsius.round(2)
+end
--- specs/ruby_functions_spec.rb
+++ specs/ruby_functions_spec.rb
@@ -98,7 +98,8 @@ class FunctionsTest < MiniTest::Test
   #Given a value in farenheit, convert this into celsius.
   def test_fahrenheit_to_celsius()
     #add test code here
+    result = fahrenheit_to_celsius(10)
+    assert_equal(-12.22, result)
   end
-
 
 end
